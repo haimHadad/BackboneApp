b1823e3c59f0b464a633c771d8ffdcc0f15c7127
משפחת חדד <haim.hddd@gmail.com>
2021-05-15 17:06:55 +0300

    Toggle an item

--- js/Models/TodoItem.js
+++ js/Models/TodoItem.js
@@ -1,6 +1,13 @@
 var TodoItem = Backbone.Model.extend({
+
+    defaults:{
+        isCompleted: false
+    },
     validate: function(attrs){
         if(!attrs.description)
         return "Description is requierd.";
+    },
+    toggle: function(){
+        this.set("isCompleted", !this.get("isCompleted"));
     }
-})
+});
