7ebaae7ac069384fc37d663340116e390df5eb37
משפחת חדד <haim.hddd@gmail.com>
2021-05-15 15:27:58 +0300

    Adding a New Todo Item

--- js/Views/CollectionView/TodoItemsView.js
+++ js/Views/CollectionView/TodoItemsView.js
@@ -7,10 +7,41 @@ var TodoItemsView = Backbone.View.extend({
         if(!(options && options.model)){
             throw new Error("model was not specified.");
         }
+        this.model.on("add", this.onAddTodoItem, this)
+    },
+
+    events: {
+        "click #add": "onClickAdd",
+        "keypress #newTodoItem": "onKeyPressed"
+    },
+    
+    onClickAdd: function(){
+        var $textBox = this.$("#newTodoItem");
+
+        if($textBox.val()){
+            var todoItem = new TodoItem({description: $textBox.val()});
+            this.model.add(todoItem);
+    
+            $textBox.val("")
+        }
+    },
+
+    onKeyPressed: function(e){
+        if(e.keyCode == 13){
+            this.onClickAdd();
+        }   
+    },
+
+    onAddTodoItem: function(todoItem){
+        var view = new TodoItemView({model: todoItem});
+        this.$el.append(view.render().$el);
     },
 
     render: function(){
         var self = this;
+
+        this.$el.append("<input autofocus type='text' placeholder='Enter new description' id='newTodoItem'></input>");
+        this.$el.append("<button id='add'>Add</button>");
 
         this.model.each((todoItem) => {
             var view = new TodoItemView ({model: todoItem});
